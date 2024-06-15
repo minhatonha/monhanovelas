a776eaa353aa25cf8f2f164d87bfc2b654120f44
minhatonha <167637882+minhatonha@users.noreply.github.com>
2024-06-14 22:53:22 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+
+const express = require('express');
+const corsConfig = require('./cors'); // Importa a configuração do CORS
+
+const app = express();
+
+// Aplica o middleware CORS configurado
+app.use(corsConfig);
+
+// Rotas e outras configurações do seu servidor aqui...
